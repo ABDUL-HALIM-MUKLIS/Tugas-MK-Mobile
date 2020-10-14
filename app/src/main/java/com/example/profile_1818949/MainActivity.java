9916f822716b6503f330332290ec8b9ea360be24
ABDUL-HALIM-MUKLIS <72723049+ABDUL-HALIM-MUKLIS@users.noreply.github.com>
2020-10-14 23:12:08 +0700

    1818040

---
+++ app/src/main/java/com/example/profile_1818949/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.profile_1818949;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
